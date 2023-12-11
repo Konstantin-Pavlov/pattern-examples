e64988bcffee99fe1a68759375d14886b2fd18e3
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-11 21:01:01 +0600

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String expression = "1+2+3-4"; // 2
+
+    }
+}
---
+++ src/main/java/org/example/interfaces/Expression.java
@@ -0,0 +1,5 @@
+package org.example.interfaces;
+
+public interface Expression {
+    int interpret(Expression context);
+}
