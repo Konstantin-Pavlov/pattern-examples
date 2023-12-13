fdf4dfa0ffac085a81edbe2e3ee9d7b67d7757e2
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-14 03:42:25 +0600

    added tests, found bug in Evaluate.java, line 15. Need to reverse string differently. First split, then reverse?

--- src/tests/InterpreterTest/InterpreterTests.java
+++ src/tests/InterpreterTest/InterpreterTests.java
@@ -2,16 +2,37 @@ package InterpreterTest;
 
 import org.example.patterns.interpreter.Evaluate;
 import org.example.patterns.interpreter.interfaces.Expression;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 public class InterpreterTests {
     @Test
     public void test1() {
-        String expression = "1+2+3-4"; // 2
+        String expression = "1+2+3-4";
         int answer = 2;
         Expression evaluator = new Evaluate(expression);
-        Assert.assertEquals(answer, evaluator.interpret(evaluator));
+        Assertions.assertEquals(answer, evaluator.interpret(evaluator));
+    }
+    @Test
+    public void test2() {
+        String expression = "1+2+3";
+        int answer = 6;
+        Expression evaluator = new Evaluate(expression);
+        Assertions.assertEquals(answer, evaluator.interpret(evaluator));
+    }
+    @Test
+    public void test3() {
+        String expression = "1+2";
+        int answer = 3;
+        Expression evaluator = new Evaluate(expression);
+        Assertions.assertEquals(answer, evaluator.interpret(evaluator));
+    }
+    @Test
+    public void test4() {
+        String expression = "12+2";
+        int answer = 12;
+        Expression evaluator = new Evaluate(expression);
+        Assertions.assertEquals(answer, evaluator.interpret(evaluator));
     }
 }
