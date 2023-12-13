3746509b8f36565a36694739cd6bb033b23c9cdd
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-13 19:32:45 +0600

    added class Evaluate that handles the string expression and uses other classes and stack to calculate the result

---
+++ src/main/java/org/example/Evaluate.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.example.interfaces.Expression;
+import org.example.model.Number;
+import org.example.operators.Minus;
+import org.example.operators.Plus;
+
+import java.util.Stack;
+
+public class Evaluate implements Expression {
+    private final Expression evaluate;
+
+    public Evaluate(String expression) {
+        Stack<Expression> expressions = new Stack<>();
+        String reversed = new StringBuilder(expression).reverse().toString();
+        for (String number : reversed.split("\\D")) { // \\D -> any symbol thar is not digit (so only digits remain in reversed.split)
+            expressions.push(new Number(Integer.parseInt(number)));
+        }
+        for (String operator : expression.split("\\d+")) { // \\d -> any symbol thar is digit (so only operators (+-) remain in reversed.split)
+            if (operator.equals("+")) {
+                expressions.push(new Plus(expressions.pop(), expressions.pop()));
+            } else if (operator.equals("-")) {
+                expressions.push(new Minus(expressions.pop(), expressions.pop()));
+            }
+        }
+
+        evaluate = expressions.pop();
+    }
+
+
+    @Override
+    public int interpret(Expression context) {
+        return evaluate.interpret(context);
+    }
+}
