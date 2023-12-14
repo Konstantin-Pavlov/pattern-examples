c05077489a06589773f966640a2852fb740c5cd1
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-14 21:57:32 +0600

    edited constructor, now the status string variable is initialized correctly

--- src/main/java/org/example/patterns/state/trucks/enums/TruckStates.java
+++ src/main/java/org/example/patterns/state/trucks/enums/TruckStates.java
@@ -3,9 +3,10 @@ package org.example.patterns.state.trucks.enums;
 public enum TruckStates {
     ON_BASE("returned to base"), ON_PARKING("Stopped in the parking lot"), ON_REPAIR("arrived for repairs"), ON_ROUTE("hit the road");
 
-    String status;
+    final String status;
 
-    TruckStates(String s) {
+    TruckStates(String status) {
+        this.status = status;
     }
 
     public String getStatus() {
