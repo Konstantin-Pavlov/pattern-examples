36bb6f7271676f81b4459914160aceacc836716c
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-14 18:19:04 +0600

    minor formatting

--- src/main/java/org/example/patterns/state/caps/CapsRunner.java
+++ src/main/java/org/example/patterns/state/caps/CapsRunner.java
@@ -1,14 +1,16 @@
 package org.example.patterns.state.caps;
 
 import org.example.patterns.state.caps.model.Name;
+import org.example.patterns.state.caps.state.CapitalizeState;
 import org.example.patterns.state.caps.state.LowerCaseState;
 import org.example.patterns.state.caps.state.UpperCaseState;
 
 public class CapsRunner {
     public static void main(String[] args) {
-        Name name = new Name("Tim");
+        Name name = new Name("tiM");
 
         System.out.println(name.getName());
+
         name.setState(new LowerCaseState());
         name.changeCase();
         System.out.println(name.getName());
@@ -17,5 +19,8 @@ public class CapsRunner {
         name.changeCase();
         System.out.println(name.getName());
 
+        name.setState(new CapitalizeState());
+        name.changeCase();
+        System.out.println(name.getName());
     }
 }
