fec4d25b4d72ff9f202ba519977cba3ca4de031e
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-10 12:46:04 +0600

    remove useless comments and imports; move tests to tests package

--- src/main/java/org/example/patterns/interpreter/Main.java
+++ src/main/java/org/example/patterns/interpreter/Main.java
@@ -1,19 +1,7 @@
 package org.example.patterns.interpreter;
 
-import org.example.patterns.interpreter.interfaces.Expression;
-
-//todo: работает только и однозначными числами
 public class Main {
     public static void main(String[] args) {
-        String expression = "1+2+3-4"; // 2
-        Expression evaluator = new Evaluate(expression);
-        System.out.println(evaluator.interpret(evaluator));
-        // how to handle whitespaces etc
-//        String reversed = new StringBuilder(expression).reverse().toString();
-//        for (String s : reversed.split("\\d+\\s*")) { // \\d -> any symbol thar is not digit (so only digits remain in reversed.split)
-//            System.out.println(s);
-//        }
-
     }
 }
 
