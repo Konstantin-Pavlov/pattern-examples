76f7f57d7faed86660c61bba09290d486dc28b89
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-10 12:49:29 +0600

    add new tests - to test new functionality with multiple digits numbers

--- src/tests/InterpreterTest/InterpreterTests.java
+++ src/tests/InterpreterTest/InterpreterTests.java
@@ -14,6 +14,7 @@ public class InterpreterTests {
         Expression evaluator = new Evaluate(expression);
         Assertions.assertEquals(answer, evaluator.interpret(evaluator));
     }
+
     @Test
     public void test2() {
         String expression = "1+2+3";
@@ -21,6 +22,7 @@ public class InterpreterTests {
         Expression evaluator = new Evaluate(expression);
         Assertions.assertEquals(answer, evaluator.interpret(evaluator));
     }
+
     @Test
     public void test3() {
         String expression = "1+2";
@@ -28,10 +30,27 @@ public class InterpreterTests {
         Expression evaluator = new Evaluate(expression);
         Assertions.assertEquals(answer, evaluator.interpret(evaluator));
     }
+
     @Test
     public void test4() {
         String expression = "12+2";
-        int answer = 12;
+        int answer = 14;
+        Expression evaluator = new Evaluate(expression);
+        Assertions.assertEquals(answer, evaluator.interpret(evaluator));
+    }
+
+    @Test
+    public void test5() {
+        String expression = "11 +22- 3 - 10  ";
+        int answer = 20;
+        Expression evaluator = new Evaluate(expression);
+        Assertions.assertEquals(answer, evaluator.interpret(evaluator));
+    }
+
+    @Test
+    public void test6() {
+        String expression = "1 +2 -13-32+   84";
+        int answer = 42;
         Expression evaluator = new Evaluate(expression);
         Assertions.assertEquals(answer, evaluator.interpret(evaluator));
     }
